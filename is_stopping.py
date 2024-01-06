744579f516ae37f52683928ef90ad1d2bf18e689
Hiroshi Matsuda <hiroshi_matsuda@megagon.ai>
2024-01-06 16:19:31 +0900

    improve by @Taishi-N324

--- is_stopping.py
+++ is_stopping.py
@@ -17,31 +17,33 @@ def job(run_path, file_path):
         with open(file_path, "r") as f:
             lines = [line.rstrip() for line in f.readlines()]
             lines = list(map(lambda line: list(map(float, line.split(","))), lines))
-            if len(lines) == 3:
-                if run.summary.get("_step", 0) == lines[-1][0]:
-                    prev_step_per_sec = (lines[1][0] - lines[0][0]) / (
-                        lines[1][1] - lines[0][1]
-                    )
+        if len(lines) == 3:
+            if run.summary.get("_step", 0) == lines[-1][0]:
+                prev_step_per_sec = (lines[1][0] - lines[0][0]) / (
+                    lines[1][1] - lines[0][1]
+                )
 
-                    now_step_per_sec = (run.summary.get("_step", 0) - lines[1][0]) / (
-                        time.time() - lines[1][1]
-                    )
+                now_step_per_sec = (run.summary.get("_step", 0) - lines[1][0]) / (
+                    time.time() - lines[1][1]
+                )
 
-                    if now_step_per_sec * 3 < prev_step_per_sec:
-                        webhook_url = "your/webhook/url"
-                        text = f"{run.name}の実行が終了または不明な理由で停止しました"
-                        requests.post(webhook_url, data=json.dumps({"text": text}))
-                        global flag
-                        flag = False
-                else:
-                    with open(file_path, "w") as f:
-                        f.write(f"{lines[1][0]},{lines[1][1]}\n")
-                        f.write(f"{lines[2][0]},{lines[2][1]}\n")
-                        f.write(f"{run.summary.get('_step', 0)},{time.time()}")
+                if now_step_per_sec * 4 < prev_step_per_sec:
+                    webhook_url = "your/webhook/url"
+                    text = f"<!channel>\n{run.name}の実行が終了または不明な理由で停止しました"
+                    requests.post(webhook_url, data=json.dumps({"text": text}))
+                    global flag
+                    flag = False
             else:
-                if run.summary.get("_step", 0) != lines[-1][0]:
-                    with open(file_path, "a") as f:
-                        f.write(f"\n{run.summary.get('_step', 0)},{time.time()}")
+                with open(file_path, "w") as f:
+                    f.write(f"{lines[1][0]},{lines[1][1]}\n")
+                    f.write(f"{lines[2][0]},{lines[2][1]}\n")
+                    f.write(f"{run.summary.get('_step', 0)},{time.time()}")
+        else:
+            if run.summary.get("_step", 0) != lines[-1][0]:
+                with open(file_path, "w") as f:
+                    for i in range(len(lines)):
+                        f.write(f"{lines[i][0]},{lines[i][1]}\n")
+                    f.write(f"{run.summary.get('_step', 0)},{time.time()}")
 
     else:
         with open(file_path, "w") as f:
@@ -50,7 +52,7 @@ def job(run_path, file_path):
 
 if __name__ == "__main__":
     run_path = "your/wandb/run/path"
-    file_path = "log/txt/file"
+    file_path = "./log.txt"
 
     if os.path.isfile(file_path):
         subprocess.call(["rm", file_path])
